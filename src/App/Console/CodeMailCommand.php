f2f86d4bfeb6c917558ca03e18c430eb8d37b654
Julien Houvion <julien.houvion@wengo.com>
2016-09-20 13:25:53 +0200

    ajout du sujet dans la generation d un mail

--- src/App/Console/CodeMailCommand.php
+++ src/App/Console/CodeMailCommand.php
@@ -99,66 +99,15 @@ class CodeMailCommand extends CodeCommand
             $class->addProperty('view', $text)->enableProtected();
         }
 
-        // SUBJECT
-
-
-        $class->write();
-
-        dd($class);
-
-/*
-        // RULER
-        $rulers = Maker::findClasses(app_path('Models/Acl'));
-        $rulerClass = $this->choice('Quelle est la classe de gestion des Acl?', $rulers, array_search('\\' . configurator()->get('ruler.class'), $rulers));
-        $ruler = Maker::load($rulerClass);
-        $this->setRuler($ruler);
-
-        // NOM
-        $nice_permission = str_replace('.', '_', $permission);
-        $constant = $this->ask('Nom de la constante?', 'PERMISSION_' . strtoupper($nice_permission));
-        $constant = strtoupper($constant);
-
-        // MIGRATION : INIT
-        $migration = $this->migration($filesystem, $nice_permission);
-
-        // ANALYSE DES CONSTANTES
-        foreach ($ruler->getConstants() as $name => $value) {
-            if ($value == $permission) {
-                // si on remarque que la permission existe deja
-                exc('La permission "' . $permission . '" existe déjà avec le nom : ' . $name);
-            } elseif ($name == $constant) {
-                // si on remarque que la permission existe deja
-                exc('Le nom "' . $permission . '" existe déjà avec la permission : ' . $value);
-            }
+        // SUJET
+        $subject = $this->ask('Ajouter un sujet?', static::CHOICE_NO_MORE);
+        if ($subject != static::CHOICE_NO_MORE) {
+            $class->addProperty('subject', $subject)->enableProtected();
         }
 
-        // LABEL
-        $label = strrpos($permission, '.');
-        $label = $label ? substr($permission, $label + 1) : $permission;
-        $label = $this->ask('Quelle est le libellé de cette Permission?', ucfirst($label));
+        // ecriture de la classe
+        $class->write();
 
-        // création de la constante
-        $ruler->addConstant($constant, $permission);
-
-        // INTERFACE
-        $interfaces = $ruler->getInterfacesConstants();
-        $interface = $this->choice('A quelle interface voulez vous rattacher cette permission?', array_values($interfaces), 0);
-        $interface = array_search($interface, $interfaces);
-
-        // GROUPE
-        $group = $this->group();
-
-        // MAJ RULER
-        $ruler->write();
-
-        // Inscirption de la migration
-        $up = $migration->getMethod('up');
-        $up->appendBody(sprintf('Acl::createDatabasePermission(Acl::%s, Acl::%s, Acl::%s, \'%s\');', $constant, $group, $interface, $label));
-        $migration->write();
-
-        // RELOAD COMPOSER
-        $composer->dumpAutoloads();
-*/
         $this->info('Have fun!!');
     }
 }
