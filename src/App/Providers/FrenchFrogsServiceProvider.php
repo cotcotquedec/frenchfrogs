40a0e47bb291d4aecee950c66f23b566f33be67c
Julien Houvion <julien.houvion@wengo.com>
2016-10-03 13:59:28 +0200

    fix migration to martavel 5.3 routing session middleware

--- src/App/Providers/FrenchFrogsServiceProvider.php
+++ src/App/Providers/FrenchFrogsServiceProvider.php
@@ -5,7 +5,7 @@ use Illuminate\Mail;
 use FrenchFrogs;
 use Response, Request, Route, Input, Blade, Auth;
 
-class FrenchFrogsServiceProvider  extends ServiceProvider
+class FrenchFrogsServiceProvider extends ServiceProvider
 {
 
 
@@ -18,7 +18,6 @@ class FrenchFrogsServiceProvider  extends ServiceProvider
         $this->bootDatatable();
         $this->bootModal();
         $this->bootValidator();
-        $this->bootMail();
     }
 
     /**
@@ -28,19 +27,9 @@ class FrenchFrogsServiceProvider  extends ServiceProvider
      */
     public function register()
     {
-        foreach(config('frenchfrogs') as $namespace => $config) {
+        foreach (config('frenchfrogs') as $namespace => $config) {
             configurator($namespace)->merge($config);
         }
-    }
-
-    /**
-     *
-     *
-     *
-     */
-    public function bootMail()
-    {
-        
     }
 
     /**
@@ -50,8 +39,7 @@ class FrenchFrogsServiceProvider  extends ServiceProvider
     public function bootValidator()
     {
         // notExists (database) renvoie true si l'entrée n'existe pas
-        \Validator::extend('not_exists', function($attribute, $value, $parameters)
-        {
+        \Validator::extend('not_exists', function ($attribute, $value, $parameters) {
             $row = \DB::table($parameters[0])->where($parameters[1], '=', $value)->first();
             return empty($row);
         });
@@ -64,81 +52,90 @@ class FrenchFrogsServiceProvider  extends ServiceProvider
     public function bootDatatable($route = '/datatable/{token}')
     {
 
-        // gestion de la navifgation Ajax
-        Route::get($route, function($token) {
+        Route::group([
+            'middleware' => [
+                \App\Http\Middleware\EncryptCookies::class,
+                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                \Illuminate\Session\Middleware\StartSession::class,
+            ],
+        ], function ($router) use ($route) {
 
-            try{
-                $request = request();
+            // gestion de la navigation Ajax
+            Route::get('/datatable/{token}', function ($token) {
 
-                // chargement de l'objet
+                try {
+                    $request = request();
+
+                    // chargement de l'objet
+                    $table = FrenchFrogs\Table\Table\Table::load($token);
+
+                    // configuration de la navigation
+                    $table->setItemsPerPage(Input::get('length'));
+                    $table->setPageFromItemsOffset(Input::get('start'));
+                    $table->setRenderer(new FrenchFrogs\Table\Renderer\Remote());
+
+                    // gestion des reccherches
+                    foreach (request()->get('columns') as $c) {
+                        if ($c['searchable'] == "true" && $c['search']['value'] != '') {
+                            $table->getColumn($c['name'])->getStrainer()->call($table, $c['search']['value']);
+                        }
+                    }
+
+                    // gestion de la recherche globale
+                    $search = $request->get('search');
+                    if (!empty($search['value'])) {
+                        $table->search($search['value']);
+                    }
+
+                    // gestion du tri
+                    $order = $request->get('order');
+                    if (!empty($order)) {
+
+                        if ($table->isSourceQueryBuilder()) {
+                            $table->getSource()->orders = [];
+                        }
+
+                        foreach ($order as $o) {
+                            extract($o);
+                            $table->getColumnByIndex($column)->order($dir);
+                        }
+                    }
+
+                    // recuperation des données
+                    $data = [];
+                    foreach ($table->render() as $row) {
+                        $data[] = array_values($row);
+                    }
+
+                    return response()->json(['data' => $data, 'draw' => Input::get('draw'), 'recordsFiltered' => $table->getItemsTotal(), 'recordsTotal' => $table->getItemsTotal()]);
+
+                } catch (\Exception $e) {
+                    //Si on catch une erreur on renvoi une reponse json avec le code 500
+                    return response()->json(['error' => $e->getMessage()], 500);
+                }
+
+            })->name('datatable');
+
+
+            /**
+             * Gestion de l'export CSV
+             */
+            Route::get('/datatable/{token}/export', function ($token) {
                 $table = FrenchFrogs\Table\Table\Table::load($token);
-
-                // configuration de la navigation
-                $table->setItemsPerPage(Input::get('length'));
-                $table->setPageFromItemsOffset(Input::get('start'));
-                $table->setRenderer(new FrenchFrogs\Table\Renderer\Remote());
-
-                // gestion des reccherches
-                foreach(request()->get('columns') as $c) {
-                    if ($c['searchable'] == "true" && $c['search']['value'] != '') {
-                        $table->getColumn($c['name'])->getStrainer()->call($table, $c['search']['value']);
-                    }
-                }
-
-                // gestion de la recherche globale
-                $search = $request->get('search');
-                if (!empty($search['value']) ) {
-                    $table->search($search['value']);
-                }
-
-                // gestion du tri
-                $order = $request->get('order');
-                if (!empty($order)) {
-
-                    if ($table->isSourceQueryBuilder()) {
-                        $table->getSource()->orders = [];
-                    }
-
-                    foreach($order as $o) {
-                        extract($o);
-                        $table->getColumnByIndex($column)->order($dir);
-                    }
-                }
-
-                // recuperation des données
-                $data = [];
-                foreach($table->render() as $row){
-                    $data[] = array_values($row);
-                }
-
-                return response()->json(['data' => $data, 'draw' => Input::get('draw'), 'recordsFiltered' => $table->getItemsTotal(), 'recordsTotal' => $table->getItemsTotal()]);
-
-            }catch (\Exception $e) {
-                //Si on catch une erreur on renvoi une reponse json avec le code 500
-                return response()->json(['error' => $e->getMessage()],500);
-            }
-
-        })->name('datatable');
+                $table->setItemsPerPage(5000);
+                $table->toCsv();
+            })->name('datatable-export');
 
 
-        /**
-         * Gestion de l'export CSV
-         */
-        Route::get($route . '/export', function($token) {
-            $table = FrenchFrogs\Table\Table\Table::load($token);
-            $table->setItemsPerPage(5000);
-            $table->toCsv();
-        })->name('datatable-export');
-
-
-        /**
-         * Gestion de l'edition en remote
-         *
-         */
-        Route::post($route, function($token) {
-            $request = request();
-            $table = FrenchFrogs\Table\Table\Table::load($token);
-            return $table->getColumn($request->get('column'))->remoteProcess($request->get('id'), $request->get('value'));
+            /**
+             * Gestion de l'edition en remote
+             *
+             */
+            Route::post('/datatable/{token}', function ($token) {
+                $request = request();
+                $table = FrenchFrogs\Table\Table\Table::load($token);
+                return $table->getColumn($request->get('column'))->remoteProcess($request->get('id'), $request->get('value'));
+            });
         });
 
     }
@@ -151,18 +148,17 @@ class FrenchFrogsServiceProvider  extends ServiceProvider
      */
     public function bootModal()
     {
-        Response::macro('modal', function($title, $body = '', $actions  = [])
-        {
+        Response::macro('modal', function ($title, $body = '', $actions = []) {
             if ($title instanceof FrenchFrogs\Modal\Modal\Modal) {
                 $modal = $title->enableRemote();
-            } elseif($title instanceof FrenchFrogs\Form\Form\Form) {
+            } elseif ($title instanceof FrenchFrogs\Form\Form\Form) {
                 $renderer = configurator()->get('form.renderer.modal.class');
                 $modal = $title->setRenderer(new $renderer());
             } else {
                 $modal = \modal($title, $body, $actions)->enableRemote();
             }
 
-            $modal .= '<script>jQuery(function() {'.js('onload').'});</script>';
+            $modal .= '<script>jQuery(function() {' . js('onload') . '});</script>';
 
             return $modal;
         });
