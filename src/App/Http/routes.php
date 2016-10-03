d5b3b946817aba56100bcbfb1b9986d5534e83ff
Julien Houvion <julien.houvion@wengo.com>
2016-10-03 16:13:34 +0200

    isolation des routes

--- src/App/Http/routes.php
+++ src/App/Http/routes.php
@@ -1,5 +1,83 @@
 <?php
 
+// gestion de la navigation Ajax
+Route::get('/datatable/{token}', function ($token) {
+
+    try {
+        $request = request();
+
+        // chargement de l'objet
+        $table = FrenchFrogs\Table\Table\Table::load($token);
+
+        // configuration de la navigation
+        $table->setItemsPerPage(Input::get('length'));
+        $table->setPageFromItemsOffset(Input::get('start'));
+        $table->setRenderer(new FrenchFrogs\Table\Renderer\Remote());
+
+        // gestion des reccherches
+        foreach (request()->get('columns') as $c) {
+            if ($c['searchable'] == "true" && $c['search']['value'] != '') {
+                $table->getColumn($c['name'])->getStrainer()->call($table, $c['search']['value']);
+            }
+        }
+
+        // gestion de la recherche globale
+        $search = $request->get('search');
+        if (!empty($search['value'])) {
+            $table->search($search['value']);
+        }
+
+        // gestion du tri
+        $order = $request->get('order');
+        if (!empty($order)) {
+
+            if ($table->isSourceQueryBuilder()) {
+                $table->getSource()->orders = [];
+            }
+
+            foreach ($order as $o) {
+                extract($o);
+                $table->getColumnByIndex($column)->order($dir);
+            }
+        }
+
+        // recuperation des données
+        $data = [];
+        foreach ($table->render() as $row) {
+            $data[] = array_values($row);
+        }
+
+        return response()->json(['data' => $data, 'draw' => Input::get('draw'), 'recordsFiltered' => $table->getItemsTotal(), 'recordsTotal' => $table->getItemsTotal()]);
+
+    } catch (\Exception $e) {
+        //Si on catch une erreur on renvoi une reponse json avec le code 500
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+
+})->name('datatable');
+
+
+/**
+ * Gestion de l'export CSV
+ */
+Route::get('/datatable/{token}/export', function ($token) {
+    $table = FrenchFrogs\Table\Table\Table::load($token);
+    $table->setItemsPerPage(5000);
+    $table->toCsv();
+})->name('datatable-export');
+
+
+/**
+ * Gestion de l'edition en remote
+ *
+ */
+Route::post('/datatable/{token}', function ($token) {
+    $request = request();
+    $table = FrenchFrogs\Table\Table\Table::load($token);
+    return $table->getColumn($request->get('column'))->remoteProcess($request->get('id'), $request->get('value'));
+});
+
+
 Route::get('/media/{id}', 'FrenchFrogs\App\Http\Controllers\MediaController@show')->name('media-show');
 Route::get('/media/dl/{id}', 'FrenchFrogs\App\Http\Controllers\MediaController@download')->name('media-dl');
 
--- src/App/Providers/FrenchFrogsServiceProvider.php
+++ src/App/Providers/FrenchFrogsServiceProvider.php
@@ -15,7 +15,6 @@ class FrenchFrogsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->bootDatatable();
         $this->bootModal();
         $this->bootValidator();
     }
@@ -43,101 +42,6 @@ class FrenchFrogsServiceProvider extends ServiceProvider
             $row = \DB::table($parameters[0])->where($parameters[1], '=', $value)->first();
             return empty($row);
         });
-    }
-
-    /**
-     * Datatable render
-     * @param string $route
-     */
-    public function bootDatatable($route = '/datatable/{token}')
-    {
-
-        Route::group([
-            'middleware' => [
-                \App\Http\Middleware\EncryptCookies::class,
-                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-                \Illuminate\Session\Middleware\StartSession::class,
-            ],
-        ], function ($router) use ($route) {
-
-            // gestion de la navigation Ajax
-            Route::get('/datatable/{token}', function ($token) {
-
-                try {
-                    $request = request();
-
-                    // chargement de l'objet
-                    $table = FrenchFrogs\Table\Table\Table::load($token);
-
-                    // configuration de la navigation
-                    $table->setItemsPerPage(Input::get('length'));
-                    $table->setPageFromItemsOffset(Input::get('start'));
-                    $table->setRenderer(new FrenchFrogs\Table\Renderer\Remote());
-
-                    // gestion des reccherches
-                    foreach (request()->get('columns') as $c) {
-                        if ($c['searchable'] == "true" && $c['search']['value'] != '') {
-                            $table->getColumn($c['name'])->getStrainer()->call($table, $c['search']['value']);
-                        }
-                    }
-
-                    // gestion de la recherche globale
-                    $search = $request->get('search');
-                    if (!empty($search['value'])) {
-                        $table->search($search['value']);
-                    }
-
-                    // gestion du tri
-                    $order = $request->get('order');
-                    if (!empty($order)) {
-
-                        if ($table->isSourceQueryBuilder()) {
-                            $table->getSource()->orders = [];
-                        }
-
-                        foreach ($order as $o) {
-                            extract($o);
-                            $table->getColumnByIndex($column)->order($dir);
-                        }
-                    }
-
-                    // recuperation des données
-                    $data = [];
-                    foreach ($table->render() as $row) {
-                        $data[] = array_values($row);
-                    }
-
-                    return response()->json(['data' => $data, 'draw' => Input::get('draw'), 'recordsFiltered' => $table->getItemsTotal(), 'recordsTotal' => $table->getItemsTotal()]);
-
-                } catch (\Exception $e) {
-                    //Si on catch une erreur on renvoi une reponse json avec le code 500
-                    return response()->json(['error' => $e->getMessage()], 500);
-                }
-
-            })->name('datatable');
-
-
-            /**
-             * Gestion de l'export CSV
-             */
-            Route::get('/datatable/{token}/export', function ($token) {
-                $table = FrenchFrogs\Table\Table\Table::load($token);
-                $table->setItemsPerPage(5000);
-                $table->toCsv();
-            })->name('datatable-export');
-
-
-            /**
-             * Gestion de l'edition en remote
-             *
-             */
-            Route::post('/datatable/{token}', function ($token) {
-                $request = request();
-                $table = FrenchFrogs\Table\Table\Table::load($token);
-                return $table->getColumn($request->get('column'))->remoteProcess($request->get('id'), $request->get('value'));
-            });
-        });
-
     }
 
     /**
