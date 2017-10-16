c29a29945bf39b1816827e1946b685ecde01c4d7
Julien Houvion <cotcotquedec@gmail.com>
2017-10-16 11:44:15 +0200

    fix vbalidate controller laravel 5.5

--- src/Core/FrenchFrogsController.php
+++ src/Core/FrenchFrogsController.php
@@ -38,28 +38,6 @@ trait FrenchFrogsController
         return $this->request;
     }
 
-
-    /**
-     * Surcharge de la validation pour recuperer le request
-     *
-     * @param Request $request
-     * @param array $rules
-     * @param array $messages
-     * @param array $customAttributes
-     * @return Request
-     */
-    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
-    {
-        $validator = $this->getValidationFactory()->make($request->all(), $rules, $messages, $customAttributes);
-
-        if ($validator->fails()) {
-            $this->throwValidationException($request, $validator);
-        }
-
-        return $request;
-    }
-
-
     /**
      * getter for l'utilisateur courant
      *
